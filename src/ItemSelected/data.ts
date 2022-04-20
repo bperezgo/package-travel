6687a8bc3e25806f08d757b4fa88a6aad24b3c3b
bperezgo <bsperezgo@gmail.com>
2022-04-19 22:36:52 -0500

    fix: axios dependency deleted

--- src/ItemSelected/data.ts
+++ src/ItemSelected/data.ts
@@ -1,12 +1,8 @@
-import Api from '../shared/apiService';
 import { IProps } from '../types';
 
 // Items can be Hotels, Tours or Flights, and then, many differents
 export const getItems = async (itemType: string): Promise<IProps[]> => {
   const apiUrl = process.env.API_ITEMS_URL || '';
-  const client = new Api({
-    baseURL: apiUrl,
-  });
   const res = await fetch(`${apiUrl}/api/${itemType}`);
   const data = (await res.json()) as IProps[];
   return data;
@@ -14,16 +10,12 @@ export const getItems = async (itemType: string): Promise<IProps[]> => {
 
 // When you choose an item, then is searched the information of the possible
 // packages of that Item
-export const getElementForItemSelected = (itemType: string, id: string) => {
-  const client = new Api({
-    baseURL: process.env.API_ITEMS_URL || '',
-  });
-  return client.get(`/${itemType}/${id}`, {
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers':
-        'Origin, X-Requested-With, Content-Type, Accept',
-    },
-  });
+export const getElementForItemSelected = async (
+  itemType: string,
+  id: string
+) => {
+  const apiUrl = process.env.API_ITEMS_URL || '';
+  const res = await fetch(`${apiUrl}/api/${itemType}/${id}`);
+  const data = await res.json();
+  return data;
 };
